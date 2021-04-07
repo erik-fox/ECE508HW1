f4b2f1106852dbfd1d2c3588a9d4afc83efa1a8c
Erik Fox <erfox@auto.ece.pdx.edu>
2021-04-07 10:38:12 -0700

    fixed print to one line

--- radius2area_circ.py
+++ radius2area_circ.py
@@ -1,9 +1,8 @@
-import math
+import math #import math library to have access to value of pi
 def circlefunc(radius):
-	circumf=(2*math.pi*radius)
-	area= math.pi*(radius**2)
-	print("circumference:", circumf)
-	print("area:",area)
+	circumf=(2*math.pi*radius)#calculate circumference (2(pi)r)
+	area= math.pi*(radius**2)#calculate area of circle ((pi)r^2)
+	print("circumference:", circumf, "area:",area)
 	return circumf,area
 
 circlefunc(int(input("Enter Radius: ")))
